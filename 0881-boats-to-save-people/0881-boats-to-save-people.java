eb4841de88711045f6f99028580c2c9c5f5d7e33
Laxmi Priya B <laxmipriya170705@gmail.com>
2026-02-16 23:05:32 +0530

    Time: 20 ms (81.89%), Space: 56.8 MB (8.96%) - LeetHub

---
+++ 0881-boats-to-save-people/0881-boats-to-save-people.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int numRescueBoats(int[] people, int limit) {
+        int count = 0;
+        int right = people.length-1;
+        int left = 0;
+        Arrays.sort(people);
+        while(left<=right){
+            if(people[left] + people[right] <= limit){
+                
+                left++;
+            }
+            right--;
+            count++;
+        }
+        return count;
+    }
+}
