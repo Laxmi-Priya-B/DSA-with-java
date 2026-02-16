11705b6f35ace4156f7fd90bc6ec4b54d3d65938
Laxmi Priya B <laxmipriya170705@gmail.com>
2026-02-16 22:15:24 +0530

    Time: 2 ms (90.11%), Space: 48 MB (17.82%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,19 +1,15 @@
 class Solution {
     public void moveZeroes(int[] nums) {
-        int c =0;
         int k=0;
         for(int i=0;i<nums.length;i++){
             if(nums[i]!=0){
-                nums[k] = nums[i];
+                nums[k]=nums[i];
                 k++;
             }
         }
-        c=nums.length-k;
-        int i=nums.length-1;
-        while(c>0){
-            nums[i]=0;
-            i--;
-            c--;
+        while(k<nums.length){
+            nums[k]=0;
+            k++;
         }
     }
 }
