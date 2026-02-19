a5fdc6e77713690daf30e3bafc1b976c9c8dccaa
Laxmi Priya B <laxmipriya170705@gmail.com>
2026-02-19 12:08:50 +0530

    Time: 4 ms (7.28%), Space: 43.9 MB (30.9%) - LeetHub

---
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
@@ -0,0 +1,31 @@
+class Solution {
+    HashMap<Character,String> map = new HashMap<>();
+    List<String> res = new ArrayList();
+    Solution(){
+        map.put('2',"abc");
+        map.put('3',"def");
+        map.put('4',"ghi");
+        map.put('5',"jkl");
+        map.put('6',"mno");
+        map.put('7',"pqrs");
+        map.put('8',"tuv");
+        map.put('9',"wxyz");
+    }
+    public List<String> letterCombinations(String digits) {
+        if(digits.length()<1){
+            return res;
+        }
+        helper(digits,0,"");
+        return res;
+    }
+    public void helper(String digits, int i, String curr){
+        if(i==digits.length()){
+            res.add(curr);
+            return;
+        }
+        String letters = map.get(digits.charAt(i));
+        for(int j=0;j<letters.length();j++){
+            helper(digits,i+1,curr+letters.charAt(j));
+        }
+    }
+}
