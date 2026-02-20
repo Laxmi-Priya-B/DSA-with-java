92b58002534e6d0b239615c320b5903aae1625ec
Laxmi Priya B <laxmipriya170705@gmail.com>
2026-02-20 23:18:21 +0530

    Time: 136 ms (58.48%), Space: 43.3 MB (20.46%) - LeetHub

--- 0079-word-search/0079-word-search.java
+++ 0079-word-search/0079-word-search.java
@@ -1,30 +1,25 @@
 class Solution {
-    int m;
-    int n;
     public boolean exist(char[][] board, String word) {
-        m = board.length;
-        n = board[0].length;
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                if(board[i][j]==word.charAt(0) && helper(i,j,board,word,0)){
+        for(int i=0;i<board.length;i++){
+            for(int j=0;j<board[0].length;j++){
+                if(board[i][j] == word.charAt(0) && helper(board,word,i,j,0)){
                     return true;
                 }
             }
         }
         return false;
     }
-    boolean helper(int i,int j,char[][] board,String word,int idx){
+    boolean helper(char[][] board,String word,int i,int j, int idx){
         if(idx == word.length()) return true;
-        if(i <0 || j<0  || i>= m || j>=n) return false;
-        if(board[i][j] != word.charAt(idx) || board[i][j]=='$') return false;
-        
+        if(i<0 || j<0 || i>= board.length || j>= board[0].length ) return false;
+        if(board[i][j] != word.charAt(idx) || board[i][j] == '$') return false;
         char ch = board[i][j];
         board[i][j] = '$';
-        boolean res1= helper(i+1,j,board,word,idx+1);
-        boolean res2= helper(i-1,j,board,word,idx+1);
-        boolean res3= helper(i,j+1,board,word,idx+1);
-        boolean res4= helper(i,j-1,board,word,idx+1);
+        boolean res1 = helper(board,word,i+1,j,idx+1);
+        boolean res2 = helper(board,word,i-1,j,idx+1);
+        boolean res3 = helper(board,word,i,j+1,idx+1);
+        boolean res4 = helper(board,word,i,j-1,idx+1);
         board[i][j] = ch;
-        return res1 || res2 || res3|| res4;
+        return res1 || res2 || res3 || res4;
     }
 }
