a7f37040e3983d14ef97cebde3850542558cf74a
Laxmi Priya B <laxmipriya170705@gmail.com>
2026-02-23 00:06:27 +0530

    Time: 0 ms (100%), Space: 42.8 MB (75.92%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,6 +1,7 @@
 class Solution {
     public int rob(int[] nums) {
-        int prev1 = 0, prev2 = 0;
+        int prev1 = 0;
+        int prev2 = 0;
         for (int num : nums) {
             int curr = Math.max(prev1, prev2 + num);
             prev2 = prev1;
