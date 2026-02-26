ca8231de5753d47f731764f95f77f6e51de71c07
Laxmi Priya B <laxmipriya170705@gmail.com>
2026-02-26 13:14:37 +0530

    Time: 12 ms (76.94%), Space: 47.5 MB (86.27%) - LeetHub

--- 2414-length-of-the-longest-alphabetical-continuous-substring/2414-length-of-the-longest-alphabetical-continuous-substring.java
+++ 2414-length-of-the-longest-alphabetical-continuous-substring/2414-length-of-the-longest-alphabetical-continuous-substring.java
@@ -3,7 +3,7 @@ class Solution {
         int maxlen = 1;
         int curr = 1;
         for(int i=1;i<s.length();i++){
-            if(s.charAt(i) - s.charAt(i-1) == 1){
+            if(s.charAt(i)-s.charAt(i-1)==1){
                 curr++;
             }
             else{
