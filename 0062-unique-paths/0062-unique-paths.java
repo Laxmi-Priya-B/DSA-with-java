45bc80883bc6d8c7858cf191bf2fa449c825c2fc
Laxmi Priya B <laxmipriya170705@gmail.com>
2026-02-27 03:54:41 +0530

    Time: 0 ms (100%), Space: 42.1 MB (83.03%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -1,7 +1,7 @@
 class Solution {
     public int uniquePaths(int m, int n) {
         long res = 1;
-        int N = m+n -2;
+        int N = m+n-2;
         int r = Math.min(m-1,n-1);
         for(int i=1;i<=r;i++){
             res = res*(N-r+i)/i;
