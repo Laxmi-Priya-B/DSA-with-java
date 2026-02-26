67760fe5346f8df6a53efe2b090034759c368f25
Laxmi Priya B <laxmipriya170705@gmail.com>
2026-02-26 19:30:10 +0530

    Time: 0 ms (100%), Space: 41.8 MB (93.62%) - LeetHub

---
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.java
@@ -0,0 +1,13 @@
+class Solution {
+    HashMap<Integer,Integer> map = new HashMap<>();
+    public int tribonacci(int n) {
+        if(n==0) return 0;
+        if(n<=2) return 1;
+        
+        if(map.containsKey(n)) return map.get(n);
+
+        int result = tribonacci(n-1) + tribonacci(n-2) + tribonacci(n-3);
+        map.put(n,result);
+        return result;
+    }
+}
