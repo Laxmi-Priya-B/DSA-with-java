2e5ecf10927a9721cd55295c4dd3ac95f6814948
Laxmi Priya B <laxmipriya170705@gmail.com>
2026-02-27 05:19:41 +0530

    Time: 0 ms (100%), Space: 43.4 MB (31.82%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.java
+++ 0014-longest-common-prefix/0014-longest-common-prefix.java
@@ -9,7 +9,7 @@ class Solution {
         for(int i=0;i<min.length();i++){
             char c = min.charAt(i);
             for(int j=0;j<strs.length;j++){
-                if(strs[j].charAt(i) !=c){
+                if(strs[j].charAt(i)!=c){
                     return min.substring(0,i);
                 }
             }
