0c155f8723d34229adde274ae9ff4c8eebddde69
Laxmi Priya B <laxmipriya170705@gmail.com>
2026-03-09 14:15:48 +0530

    Time: 16 ms (39.42%), Space: 47.7 MB (64.69%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -8,7 +8,7 @@ class Solution {
         list.sort((a,b) -> map.get(b) - map.get(a));
         int[] res = new int[k];
         for(int i=0;i<k;i++){
-            res[i] = list.get(i);
+            res[i]=list.get(i);
         }
         return res;
     }
