460cf08b794c10c823b2c931fe5b68bb2435f45c
Laxmi Priya B <laxmipriya170705@gmail.com>
2026-03-13 23:03:30 +0530

    Time: 1 ms (94.09%), Space: 43.5 MB (71.35%) - LeetHub

---
+++ 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.java
@@ -0,0 +1,13 @@
+class Solution {
+    public String gcdOfStrings(String str1, String str2) {
+        if(!(str1+str2).equals(str2+str1)) return "";
+
+        int res = gcd(str1.length(),str2.length());
+        return str2.substring(0,res);
+    }
+    int gcd(int p,int q){
+        if(q==0) return p;
+        
+        return gcd(q, p%q);
+    }
+}
