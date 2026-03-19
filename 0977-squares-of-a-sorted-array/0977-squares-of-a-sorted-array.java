377b8c2e48d597f57cacc6cf57224d48b7f915a7
Laxmi Priya B <laxmipriya170705@gmail.com>
2026-03-19 19:20:24 +0530

    Time: 10 ms (37.49%), Space: 47.8 MB (24%) - LeetHub

--- 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.java
+++ 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.java
@@ -3,7 +3,7 @@ class Solution {
         int left =0;
         int right = nums.length-1;
         while(left<=right){
-            if(left>right){
+            if(left<right){
                 nums[left] = nums[left]*nums[left];
                 left++;
             }
