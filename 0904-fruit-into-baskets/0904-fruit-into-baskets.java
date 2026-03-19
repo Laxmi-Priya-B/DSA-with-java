86ec693b3098aad7e7ef4d4990169ef93f7ac29c
Laxmi Priya B <laxmipriya170705@gmail.com>
2026-03-19 21:04:23 +0530

    Time: 6 ms (98.94%), Space: 73 MB (10.23%) - LeetHub

--- 0904-fruit-into-baskets/0904-fruit-into-baskets.java
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.java
@@ -1,19 +1,47 @@
 class Solution {
     public int totalFruit(int[] fruits) {
-        HashMap<Integer, Integer> map = new HashMap<>();
-        int left = 0;
+        int last = -1;
+        int secondLast = -1;
+        int lastCount = 0;
+        int curr = 0;
         int max = 0;
-        for(int right =0;right<fruits.length;right++){
-            map.put(fruits[right], map.getOrDefault(fruits[right], 0)+1);
-            while(map.size()>2){
-                map.put(fruits[left], map.get(fruits[left])-1);
-                if(map.get(fruits[left]) == 0){
-                    map.remove(fruits[left]);
-                }
-                left++;
+        for(int fruit:fruits){
+            if(fruit == last || fruit == secondLast){
+                curr++;
             }
-            max = Math.max(max, right - left+1);
+            else{
+                curr = lastCount + 1;
+            }
+            if(fruit == last){
+                lastCount++;
+            }
+            else{
+                lastCount=1;
+                secondLast = last;
+                last = fruit;
+            }
+            max = Math.max(max,curr);
         }
         return max;
     }
 }
+
+// class Solution {
+//     public int totalFruit(int[] fruits) {
+//         HashMap<Integer, Integer> map = new HashMap<>();
+//         int left = 0;
+//         int max = 0;
+//         for(int right =0;right<fruits.length;right++){
+//             map.put(fruits[right], map.getOrDefault(fruits[right], 0)+1);
+//             while(map.size()>2){
+//                 map.put(fruits[left], map.get(fruits[left])-1);
+//                 if(map.get(fruits[left]) == 0){
+//                     map.remove(fruits[left]);
+//                 }
+//                 left++;
+//             }
+//             max = Math.max(max, right - left+1);
+//         }
+//         return max;
+//     }
+// }
