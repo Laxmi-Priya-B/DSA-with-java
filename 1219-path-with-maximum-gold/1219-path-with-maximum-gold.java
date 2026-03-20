49ec7935d9ca5c816eaf2370e9b999ab2ac43c80
Laxmi Priya B <laxmipriya170705@gmail.com>
2026-03-21 02:24:43 +0530

    Time: 60 ms (89.09%), Space: 43.1 MB (39.25%) - LeetHub

--- 1219-path-with-maximum-gold/1219-path-with-maximum-gold.java
+++ 1219-path-with-maximum-gold/1219-path-with-maximum-gold.java
@@ -1,29 +1,29 @@
 class Solution {
+    int max = 0;
     public int getMaximumGold(int[][] grid) {
-        int max = 0;
+        
         for(int i=0;i<grid.length;i++){
             for(int j=0;j<grid[0].length;j++){
                 if(grid[i][j]!=0){
-                    max = Math.max(max, helper(grid,i,j));
+                    helper(grid,i,j,0);
                 }
             }
         }
         return max;
     }
 
-    int helper(int[][] grid,int i,int j){
+    void helper(int[][] grid,int i,int j,int curr){
         if(i<0 || j<0 || i>= grid.length || j>= grid[0].length || grid[i][j] == 0){
-            return 0;
+            max = Math.max(max, curr);
+            return;
         }
         int temp = grid[i][j];
         grid[i][j] = 0;
-        int max = 0;
-        max = Math.max(max, helper(grid,i+1,j));
-        max = Math.max(max, helper(grid,i-1,j));
-        max = Math.max(max, helper(grid,i,j+1));
-        max = Math.max(max, helper(grid,i,j-1));
+        helper(grid,i+1,j,curr + temp);
+        helper(grid,i-1,j,curr + temp);
+        helper(grid,i,j+1,curr + temp);
+        helper(grid,i,j-1,curr + temp);
         grid[i][j] = temp;
 
-        return max + temp;
     }
 }
