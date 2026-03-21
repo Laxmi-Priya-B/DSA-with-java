17b3d23941d4f7d678c69d1fe6458c2ca28312cd
Laxmi Priya B <laxmipriya170705@gmail.com>
2026-03-21 19:39:43 +0530

    Time: 0 ms (100%), Space: 43.5 MB (18.59%) - LeetHub

--- 1700-number-of-students-unable-to-eat-lunch/1700-number-of-students-unable-to-eat-lunch.java
+++ 1700-number-of-students-unable-to-eat-lunch/1700-number-of-students-unable-to-eat-lunch.java
@@ -1,22 +1,52 @@
+// class Solution {
+//     public int countStudents(int[] students, int[] sandwiches) {
+//         Queue<Integer> q = new LinkedList<>();
+//         for(int i:students){
+//             q.add(i);
+//         }
+//         int i = 0;
+//         int r = 0;
+//         while(!q.isEmpty() && r < q.size()){
+//             if(q.peek() == sandwiches[i]){
+//                 q.poll();
+//                 i++;
+//                 r = 0;
+//             }
+//             else{
+//                 q.add(q.poll());
+//                 r++;
+//             }
+//         }
+//         return q.size();
+//     }
+// }
+
 class Solution {
     public int countStudents(int[] students, int[] sandwiches) {
-        Queue<Integer> q = new LinkedList<>();
+        int zero = 0;
+        int one = 0;
         for(int i:students){
-            q.add(i);
-        }
-        int i = 0;
-        int r = 0;
-        while(!q.isEmpty() && r < q.size()){
-            if(q.peek() == sandwiches[i]){
-                q.poll();
-                i++;
-                r = 0;
+            if(i == 0){
+                zero++;
             }
             else{
-                q.add(q.poll());
-                r++;
+                one++;
             }
         }
-        return q.size();
+        for(int i:sandwiches){
+            if(i == 0){
+                if(zero == 0){
+                    return one;
+                }
+                zero--;
+            }
+            else{
+                if(one == 0){
+                    return zero;
+                }
+                one--;
+            }
+        }
+        return 0;
     }
 }
