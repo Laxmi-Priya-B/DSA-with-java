215462d01b536e7a57d2bd3c668b4c8ba7f2c678
Laxmi Priya B <laxmipriya170705@gmail.com>
2026-03-22 22:40:08 +0530

    Time: 11 ms (10.8%), Space: 65.2 MB (84.49%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.java
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.java
@@ -20,7 +20,6 @@ class Solution {
         }
     }
     boolean isPalindrome(String s){
-        if(s.length() == 0) return false;
         int i = 0;
         int j = s.length()-1;
         while(i<j){
