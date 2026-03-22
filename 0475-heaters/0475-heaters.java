255e0702245f12382dd803d60ea7785379fe4051
Laxmi Priya B <laxmipriya170705@gmail.com>
2026-03-22 16:36:31 +0530

    Time: 18 ms (85.15%), Space: 47.9 MB (79.8%) - LeetHub

--- 0475-heaters/0475-heaters.java
+++ 0475-heaters/0475-heaters.java
@@ -1,13 +1,32 @@
 class Solution {
     public int findRadius(int[] houses, int[] heaters) {
+        Arrays.sort(houses);
+        Arrays.sort(heaters);
+
+        int i=0;
         int res = 0;
-        for(int i:houses){
-            int min = Integer.MAX_VALUE;
-            for(int h:heaters){
-                min = Math.min(min, Math.abs(i-h));
+        for(int house:houses){
+            while (i < heaters.length - 1 && Math.abs(heaters[i + 1] - house) <= Math.abs(heaters[i] - house)) {
+                i++;
             }
-            res = Math.max(res, min);
+
+            int dist = Math.abs(heaters[i] - house);
+            res = Math.max(res, dist);
         }
         return res;
     }
 }
+
+// class Solution {
+//     public int findRadius(int[] houses, int[] heaters) {
+//         int res = 0;
+//         for(int i:houses){
+//             int min = Integer.MAX_VALUE;
+//             for(int h:heaters){
+//                 min = Math.min(min, Math.abs(i-h));
+//             }
+//             res = Math.max(res, min);
+//         }
+//         return res;
+//     }
+// }
