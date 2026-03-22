714c86850887e905b5566e6ced4c92dd6050e91f
Laxmi Priya B <laxmipriya170705@gmail.com>
2026-03-22 22:38:49 +0530

    Time: 7 ms (97.82%), Space: 65.6 MB (10.98%) - LeetHub

---
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.java
@@ -0,0 +1,34 @@
+class Solution {
+    List<List<String>> res = new ArrayList<>();
+    public List<List<String>> partition(String s) {
+        helper(s, 0, new ArrayList<>());
+        return res;
+    }
+
+    void helper(String s, int idx, List<String> list){
+        if(idx == s.length()){
+            res.add(new ArrayList<>(list));
+            return;
+        }
+        for(int i=idx;i<s.length();i++){
+            String sub = s.substring(idx,i+1);
+            if(isPalindrome(sub)){
+                list.add(sub);
+                helper(s,i+1,list);
+                list.remove(list.size()-1);
+            }
+        }
+    }
+    boolean isPalindrome(String s){
+        int i = 0;
+        int j = s.length()-1;
+        while(i<j){
+            if(s.charAt(i) != s.charAt(j)){
+                return false;
+            }
+            i++;
+            j--;
+        }
+        return true;
+    }
+}
